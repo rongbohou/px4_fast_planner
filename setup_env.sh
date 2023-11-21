59bcca408724b854c115e30d51960f1c75cd9bd3
rongbohou <rongbohou@163.com>
2023-11-21 10:48:31 +0800

    [update] setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source ./devel/setup.bash
 source ~/PX4-Autopilot/Tools/simulation/gazebo-classic/setup_gazebo.bash ~/PX4-Autopilot ~/PX4-Autopilot/build/px4_sitl_default
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/PX4-Autopilot
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic
